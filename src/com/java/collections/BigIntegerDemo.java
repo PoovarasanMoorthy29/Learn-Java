97c22702c3830b69a01aabbd70d7b20954934023
PoovarasanMoorthy <poovarasanmoorthy375@gmail.com>
2026-03-23 09:46:37 +0530

    Big integer demo class written

--- src/com/java/collections/BigIntegerDemo.java
+++ src/com/java/collections/BigIntegerDemo.java
@@ -1,4 +1,14 @@
 package com.java.collections;
 
+import java.math.BigInteger;
 public class BigIntegerDemo {
+    public static void main(String [] args){
+        String str1="2";
+        String str2="6";
+
+        BigInteger num1=new BigInteger(str1);
+        BigInteger num2=new BigInteger(str2);
+        num1=num1.multiply(num2);
+        System.out.println(num1);
+    }
 }
