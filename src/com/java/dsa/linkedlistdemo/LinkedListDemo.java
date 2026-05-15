bad2c82d1a9fe2506a8ee78722750d779d58ea26
PoovarasanMoorthy29 <poovarasanmoorthy375@gmail.com>
2026-05-15 17:10:27 +0530

    today push

--- src/com/java/dsa/linkedlistdemo/LinkedListDemo.java
+++ src/com/java/dsa/linkedlistdemo/LinkedListDemo.java
@@ -1,5 +1,7 @@
 package com.java.dsa.linkedlistdemo;
 
+import java.util.Scanner;
+
 public class LinkedListDemo {
     Node head;
     public LinkedListDemo(){
@@ -20,6 +22,53 @@ public class LinkedListDemo {
         head=newNode;
 
     }
+    public void insertAtLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return ;
+        }
+        Node current=head;
+        while(current.next!=null){
+            current=current.next;
+        }
+        current.next=newNode;
+
+
+    }
+    public void deleteAtFirst(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        head=head.next;
+    }
+    public void deleteAtLast(){
+        Node temp=head;
+        Node prev=head;
+        if(temp==null){
+            return ;
+        }
+        Node currentNode=head;
+        while(currentNode.next!=null){
+            prev=currentNode;
+            currentNode=currentNode.next;
+        }
+        prev.next=null;
+
+    }
+    public void deleteElementByValue(int index){
+        Node temp=head;
+        Node prev=null;
+        int i=1;
+        while(index!=i){
+            prev=temp;
+            temp=temp.next;
+            i++;
+        }
+        assert prev != null;
+        prev.next=temp.next;
+    }
     public void display(){
         Node temp=head;
         while(temp!=null){
@@ -33,17 +82,18 @@ public class LinkedListDemo {
             }
         }
     }
-
-
-
     public static void main(String[] args) {
         LinkedListDemo obj=new LinkedListDemo();
-        obj.insertAtBeginning(10);
-        obj.insertAtBeginning(20);
-        obj.insertAtBeginning(30);
+        obj.insertAtLast(10);
+        obj.insertAtLast(20);
+        obj.insertAtLast(30);
+
+        System.out.println("Before Deletion:");
         obj.display();
-
-
+        obj.deleteAtLast();
+        obj.deleteAtLast();
+        System.out.println("\nAfter Deletion:");
+        obj.display();
 
     }
 }
